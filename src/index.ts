64ca5705e7f35dad7006e2a31e3e4237a2ff0b98
vipuldunde <vipul.dunde@appdirect.com>
2024-08-05 16:17:36 +0530

    Test Change 1

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,13 @@ const port = process.env.SERVER_PORT;
 app.get("/express", (req: Request, res: Response) =>
 {
   console.log("Hello from Express!");
-  return res.status(200).json({ message: "Hello from Express!" });
+  return res.status(200).json({ message: "Hello from Express App!" });
 });
 
 app.get("/", (req: Request, res: Response) =>
 {
   console.log("Hello from Home!");
-  return res.status(200).json({ message: "Hello from Home!" });
+  return res.status(200).json({ message: "Hello from Home App!" });
 });
 
 app.listen(port, () => {
