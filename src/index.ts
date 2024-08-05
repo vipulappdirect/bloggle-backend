3f0569b3d96add11d4bd36a45d60e09c58d77f61
vipuldunde <vipul.dunde@appdirect.com>
2024-08-05 16:04:03 +0530

    Service Configuration.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-
 app.use(cors());
-const port = 5000;
 
-app.get("/express", (req: Request, res: Response) => {
+const port = process.env.SERVER_PORT;
+
+app.get("/express", (req: Request, res: Response) =>
+{
+  console.log("Hello from Express!");
   return res.status(200).json({ message: "Hello from Express!" });
 });
 
+app.get("/", (req: Request, res: Response) =>
+{
+  console.log("Hello from Home!");
+  return res.status(200).json({ message: "Hello from Home!" });
+});
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Express Server Started`);
 });
